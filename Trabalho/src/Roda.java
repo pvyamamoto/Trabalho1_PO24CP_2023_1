a2e5c1893952e7e482b09f4f44330687e3c3537a
mateus <mateusbusato@gmail.com>
2023-04-17 22:40:03 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ Trabalho/src/Roda.java
@@ -0,0 +1,28 @@
+package Trabalho.src;
+import java.util.Random;
+
+public class Roda {
+
+    private boolean calibragemPneu;
+
+    public Roda(){
+        Random rd = new Random();
+        this.setCalibragemPneu(rd.nextBoolean());
+    }
+
+    public String toString(){
+        if(this.isCalibragemPneu()){
+            return "O pneu esta calibrado";
+        }else{
+            return "O pneu nao esta calibrado";
+        }
+    }
+
+    public boolean isCalibragemPneu() {
+        return calibragemPneu;
+    }
+
+    public void setCalibragemPneu(boolean calibragemPneu) {
+        this.calibragemPneu = calibragemPneu;
+    }
+}
