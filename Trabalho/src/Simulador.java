4f1eee1f150e80a076a80fbf0064512f4e8b7fb5
mateus <mateusbusato@gmail.com>
2023-04-21 14:24:06 -0300

    Commit correção variaveis e contrutor da classe veiculo

--- Trabalho/src/Simulador.java
+++ Trabalho/src/Simulador.java
@@ -1,26 +1,40 @@
+
 public class Simulador {
 
     private Veiculo[] veiculos;
-    private static int qtidVeiculos;
+    private static int qtidVeiculos = 0;
 
-    public void incluirVeiculo(int id){
-
-
-
+    public void incluirVeiculo(){
+        this.setQtidVeiculos(1);// adiciona 1 no set
+        this.getVeiculos()[qtidVeiculos] = new Veiculo(); // inicializa um veiculo no espaço do do vetor
     }
 
     public void removerVeiculo(int id){
+        if(this.getVeiculos()[id] == null){
+            System.out.println("Veiculo inexistente!");
+        }else{
+            this.getVeiculos()[id] = null;
+        }
+    }
 
-
-
+    public void abastecer(int id, double quant){
+        this.getVeiculos()[id].setCombustivel(quant);
     }
 
     public void mover(int id){
 
     }
 
-    public void mover(){
+    public void moverTodos(){
 
+    }
+
+    public void imprimirPista(){
+
+    }
+
+    public String toString(){
+        return "ebaaa";
     }
 
 
@@ -30,8 +44,20 @@ public class Simulador {
     }
 
     public static void setQtidVeiculos(int qtidVeiculos) {
-        Simulador.qtidVeiculos = qtidVeiculos;
+
+        if(Simulador.getQtidVeiculos()>=20){
+            System.out.println("Impossivel completar operacao, o vetor esta cheio");
+        }else{
+            Simulador.qtidVeiculos += qtidVeiculos;  // += para acionar o espaco e nao mudar, assim progredindo pelo vetor (tem que ter tratamento para o limite de veiculos)
+        }
     }
 
 
+    public Veiculo[] getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(Veiculo[] veiculos) {
+        this.veiculos = veiculos;
+    }
 }
